8875785a75bf4d706ec3e4668d77cfafd91a94a6
krassilnikov <153736692+krassilnikov@users.noreply.github.com>
2024-01-31 12:27:08 +0300

    1
    1

--- HW2/2.py
+++ HW2/2.py
@@ -4,7 +4,7 @@ from transformers import pipeline
 
 
 st.title('RU/ENG translation')
-text_from_st = st.text_input('Text from streamlit') #ввод пользователя
+text_from_st = st.text_input('Текст для перевода') #ввод пользователя
     
 translator = pipeline("translation_ru_to_en", "Helsinki-NLP/opus-mt-ru-en")
 
