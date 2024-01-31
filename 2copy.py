fe4fc0fec7f96146243c2799df6d4c6766d208b6
krassilnikov <153736692+krassilnikov@users.noreply.github.com>
2024-01-31 16:21:03 +0300

    Update 2copy.py

--- 2copy.py
+++ 2copy.py
@@ -3,16 +3,14 @@ import torch
 import streamlit as st
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 st.title('title1')
+
+text_from_st = st.text_input('Текст') #ввод пользователя
+
 model_checkpoint = 'cointegrated/rubert-tiny-toxicity'
 tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
 model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint)
 if torch.cuda.is_available():
-    model.cuda()
-    
-
-
-text_from_st = st.text_input('Текст') #ввод пользователя
-    
+    model.cuda()   
 def text2toxicity(text, aggregate=True):
     """ Calculate toxicity of a text (if aggregate=True) or a vector of toxicity aspects (if aggregate=False)"""
     with torch.no_grad():
@@ -24,4 +22,10 @@ def text2toxicity(text, aggregate=True):
         return 1 - proba.T[0] * (1 - proba.T[-1])
     return proba
 
-text2toxicity('text_from_st', True)
+def m(Text):
+    st.write('получилось :',text2toxicity(Text)) 
+ #   st.write('получилось :') 
+
+m(text_from_st)
+#st.write(text2toxicity('text_from_st', True))
+#st.write("еукс")
