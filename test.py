c244fa279850ff6713cf913edcd23c7fd732a0a1
vskolegov <sevako011@bk.ru>
2024-06-21 07:46:29 +0300

    pep8 spaces

--- test.py
+++ test.py
@@ -2,8 +2,13 @@ from transformers import AutoModelForSequenceClassification, BertTokenizerFast
 import torch
 import streamlit_app
 
-tokenizer = BertTokenizerFast.from_pretrained('blanchefort/rubert-base-cased-sentiment-rusentiment')
-model = AutoModelForSequenceClassification.from_pretrained('blanchefort/rubert-base-cased-sentiment-rusentiment', return_dict=True)
+tokenizer = BertTokenizerFast.from_pretrained(
+    'blanchefort/rubert-base-cased-sentiment-rusentiment'
+)
+model = AutoModelForSequenceClassification.from_pretrained(
+    'blanchefort/rubert-base-cased-sentiment-rusentiment', return_dict=True
+)
+
 
 def predict_sentiment(text):
     inputs = tokenizer(text, max_length=512, padding=True, truncation=True, return_tensors='pt')
@@ -12,12 +17,12 @@ def predict_sentiment(text):
     predicted_class = torch.argmax(predicted).item()
     return predicted_class
 
-# test_streamlit_app.py
 
 def test_predict_sentiment():
     text = "This is a test text"
     predicted_class = streamlit_app.predict_sentiment(text)
     assert predicted_class in [0, 1, 2], f"Invalid predicted class: {predicted_class}"
+
 
 if __name__ == "__main__":
     test_predict_sentiment()
